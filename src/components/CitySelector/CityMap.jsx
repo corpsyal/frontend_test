5dfa7b442d57fdd265df3d49da7096e43c8e9d70
Anthony <lahlah.anthony@gmail.com>
2020-07-20 00:48:16 +0200

    feat: enable map section

--- src/components/CitySelector/CityMap.jsx
+++ src/components/CitySelector/CityMap.jsx
@@ -1,18 +1,31 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Wrapper = styled.div`
     flex: 3;
     border: 1px solid lightgrey;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+
+    .leaflet-container {
+        height: 100%;
+        width: 100%;
+    }
 `
+
+const position = [51.505, -0.09]
 
 const CityMap = () => {
     return (
         <Wrapper>
-            Map
+            <Map center={position} zoom={13}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                <Marker position={position}>
+                    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                </Marker>
+            </Map>
         </Wrapper>
     )
 }
